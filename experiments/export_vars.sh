291e0bd6b5266dccb66902389317f50cb03e06ec
ekmolloy <molloy.erin.k@gmail.com>
2021-09-30 12:51:49 -0400

    Modules available on hoffman have changed

--- experiments/export_vars.sh
+++ experiments/export_vars.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 . /u/local/Modules/default/init/modules.sh
-module load gcc/4.9.3
-module load python/3.7.2
-module load R/3.4.0
+#module load gcc/4.9.3
+#module load python/3.7.2
+#module load R/3.4.0
+module load gcc/4.9.5
+module load python/3.7.3
+module load R/3.6.1
 
 export HOMEDIR="$HOME/project-sriram/archie2/experiments"
 export EXTDIR="$HOMEDIR/external"
